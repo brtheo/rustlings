cbbe937eac1763310c129dc584cb188fce07c86f
br.Theo <brossiertheo@live.fr>
2023-04-07 22:03:24 +0200

    first commit

---
+++ exercises/primitive_types/primitive_types3.rs
@@ -0,0 +1,16 @@
+// primitive_types3.rs
+// Create an array with at least 100 elements in it where the ??? is.
+// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
+
+fn main() {
+    let mut a = Vec::<i32>::new();
+    for i in 0..100 {
+      a.push(i);
+    }
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
